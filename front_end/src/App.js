9d2a2489c7516a6da03346e642c278e535831996
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-10-06 17:44:39 -0600

    initialized tailwind :)

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1 className="text-3xl font-bold underline m-10">
+      Hey guys! This was styled with Tailwind CSS!
+      <br /> <br />
+      - Omar
+    </h1>
+  )
 }
 
 export default App;
