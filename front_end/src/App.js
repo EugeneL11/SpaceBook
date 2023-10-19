1771aa8f3731065f3d52c22dafa83affe38d88e9
Victor Han <victor.han72@gmail.com>
2023-10-19 07:42:47 -0600

    Test Basic design pattern
    Figured out how to get two child nodes with a common parent to communicate

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,16 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import FetchTest from './FetchTest'
+import C1 from './C1'
+import C2 from './C2'
+import { useState } from 'react';
 function App() {
+  const [num, setNum] = useState(0)
   return (
-    <div>
+    /*<div>
     <h1 className="text-3xl font-bold underline m-10">
       Hey guys! This was styled with Tailwind CSS!
       <br /> <br />
       - Omar
     </h1>
     <FetchTest/>
-    </div>
+    </div>*/
+    <div> <C1 num = {num}></C1>
+    <C2 num = {num} setNum = {setNum}/></div>
+   
   )
 }
 
