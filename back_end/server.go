836e3c38119a56b85cb3f5ff2caf00ef19a02c65
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-10-23 09:54:47 -0600

    Add gin CORS

--- back_end/server.go
+++ back_end/server.go
@@ -1,17 +1,26 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 const PORT_NO = ":8080"
 
+// Managing proxies: https://pkg.go.dev/github.com/gin-gonic/gin#section-readme
+// CORS: https://github.com/gin-contrib/cors
+
 func main() {
 	// Using Gin for the server:
 	server := gin.Default()
+	// config := cors.DefaultConfig()
+	// config.AllowOrigins = []string{URLs Go here}
 	server.GET("/ping", pong)
 	server.PUT("/num")
+	server.Use(cors.Default()) // This allows all origins
+	// server.Use(cors.New(config))
 	server.Run(PORT_NO)
 }
 
