13587c4ae52edacede42ff971693bdf3c90039e6
EugeneL11 <eugenelee0605@gmail.com>
2023-10-30 14:50:39 -0600

    Test branch "eugene"

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,15 +1,18 @@
 import FetchTest from "./FetchTest";
 import Background from "./Background/background.jsx";
+import Login from "./Login/login";
 
 function App() {
     return (
         <>  
-            <Background></Background>
+            {/* <Background></Background>
             <h1 className="text-3xl font-bold underline">
                 Hey guys! This was styled with Tailwind CSS!
                 <br /> <br /> - Omar
-            </h1>
+            </h1> */}
             {/* <FetchTest/> */}
+
+            <Login></Login>
         </>
     );
 }
