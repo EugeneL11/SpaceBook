e9da397efa2f1b550636c6abe6de7ec0b0a4ddb7
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-10-29 19:07:38 -0600

    cleaned up 🤓

--- front_end/src/Background/background.jsx
+++ front_end/src/Background/background.jsx
@@ -1,10 +1,8 @@
-import { useRef } from "react";
 import {
-    //   useHelper,
     Sparkles,
     Stars,
   } from "@react-three/drei";
-import { useThree, Canvas } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 
 export default function Background() {
     const colorArray = [
@@ -16,32 +14,28 @@ export default function Background() {
 
     return (
         <div className="h-screen w-screen fixed">
-
         <Canvas>
     
-          <Sparkles
-            /** Number of particles (default: 100) */
-            count={count}
-            /** Speed of particles (default: 1) */
-            speed={0.5}
-            /** Opacity of particles (default: 1) */
-            // opacity?: number | Float32Array
-            /** Color of particles (default: 100) */
-            // random color
-            color={ "#fffa86" }
-            /** Size of particles (default: randomized between 0 and 1) */
-            size={3}
-            /** The space the particles occupy (default: 1) */
-            scale={10}
-            /** Movement factor (default: 1) */
-            // noise?: number | [number, number, number] | THREE.Vector3 | Float32Array
-          />
+            <Sparkles
+                /** Number of particles (default: 100) */
+                count={count}
+                /** Speed of particles (default: 1) */
+                speed={0.5}
+                /** Opacity of particles (default: 1) */
+                // opacity?: number | Float32Array
+                /** Color of particles (default: 100) */
+                color={ "#fffa86" }
+                /** Size of particles (default: randomized between 0 and 1) */
+                size={3}
+                /** The space the particles occupy (default: 1) */
+                scale={10}
+                /** Movement factor (default: 1) */
+                // noise?: number | [number, number, number] | THREE.Vector3 | Float32Array
+            />
 
-        <Stars radius={1} depth={50} count={5000} factor={4} saturation={0} fade speed={5} />
-
+            <Stars radius={1} depth={50} count={5000} factor={4} saturation={0} fade speed={5} />
 
         </Canvas>  
         </div>
-
-      );
-    }
+    );
+}
