8d85228d7d73f9108dd58971bf9eb7d6d7b3ae17
EugeneL11 <eugenelee0605@gmail.com>
2023-10-30 19:03:52 -0600

    start DM messages

--- front_end/src/Messages/DMMessage.jsx
+++ front_end/src/Messages/DMMessage.jsx
@@ -3,11 +3,13 @@ import backPic from './back.png';
 
 function DMMessage(props) {
     return (
-    <div>
-        <div class="bg-pink-900 py-4 flex justify-center"> 
-            <img></img>
-            <div class="text-white text-3xl">kevonosdiaz</div>
+    <div class="flex flex-col min-h-screen">
+        <div class="bg-pink-900 py-5 flex justify-center items-center"> 
+            <img src={backPic} alt="a back arrow button" class="w-10 mr-auto pl-3"></img>
+            <div class="text-white text-3xl absolute ">kevonosdiaz</div>
         </div>
+
+        <div class="flex-1 bg-gray-700"></div>
     </div>
     );
 
