8e03ed2ca957cf950933b286d0fe88f7794d9753
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-11-01 14:50:39 -0600

    Added earth (50mins) 🌎

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,10 +1,12 @@
 import FetchTest from "./FetchTest";
 import Background from "./Background/background.jsx";
+import MyProfile from "./Profile/MyProfile";
 
 function App() {
     return (
         <>  
             <Background></Background>
+            <MyProfile></MyProfile>
             <h1 className="text-3xl font-bold underline">
                 Hey guys! This was styled with Tailwind CSS!
                 <br /> <br /> - Omar
--- front_end/src/Profile/MyProfile.jsx
+++ front_end/src/Profile/MyProfile.jsx
@@ -1,8 +1,13 @@
 import { React, useState } from "react";
+import Planet from "./Planet.jsx";
+import { Canvas } from "@react-three/fiber";
 
 function MyProfile(props) {
-    return (<div>This is the My Profile component</div>);
-
+    return (
+        <Canvas className="cursor-pointer">
+            <Planet></Planet>
+        </Canvas>
+    );
 }
 
 export default MyProfile;
