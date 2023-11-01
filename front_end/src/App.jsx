5cd31a85597815fd62e7df5998aa2c586bb3bdd6
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-10-31 18:19:02 -0600

    Removed testing code from App.jsx

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,16 +1,14 @@
 import FetchTest from "./FetchTest";
 import Background from "./Background/background.jsx";
-import ExpandedPost from "./Posts/ExpandedPost.jsx";
 
 function App() {
     return (
         <>
-            <ExpandedPost />
             <Background></Background>
-            {/* <h1 className="text-3xl font-bold underline">
+            <h1 className="text-3xl font-bold underline">
                 Hey guys! This was styled with Tailwind CSS!
                 <br /> <br /> - Omar
-            </h1> */}
+            </h1>
             {/* <FetchTest/> */}
         </>
     );
