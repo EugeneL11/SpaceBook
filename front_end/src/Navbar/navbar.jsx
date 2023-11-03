022f108543c1c87107127f8ab9b07e4911cdb977
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-11-02 18:05:46 -0600

    made navbar [4 hrs]

--- front_end/src/Navbar/navbar.jsx
+++ front_end/src/Navbar/navbar.jsx
@@ -7,14 +7,54 @@ function Navbar(props) {
     const toggleDMList = props.clickHandlers.toggleDMList;
     const toggleSearchUser = props.clickHandlers.toggleSearchUser;
     const toggleNotifications = props.clickHandlers.toggleNotifications;
-    return (<div className="flex flex-row justify-between">
-        <div onClick={toggleHomepage}>Logo(go homepage)</div>
-        <div onClick={toggleNewPost}>New Post</div>
-        <div onClick={toggleSearchUser}> Search users</div>
-        <div onClick={toggleNotifications}> Notifications</div>
-        <div onClick={toggleDMList}>Direct Messages</div>
-        <div onClick={toggleProfile}>My Profile</div>
-    </div>);
+
+return (
+  <div className="flex flex-row justify-around bg-gradient-to-r from-indigo-300 to-purple-300 fixed w-screen">
+
+    <div onClick={toggleHomepage} className="flex flex-row items-center hover:scale-90 transition-all ease-in-out align-middle cursor-pointer">
+        <img
+        src="./logo.png"
+        className="w-12"
+        />
+        <h1 className="text-black hidden md:block text-lg">&nbsp; SpaceBook</h1>
+    </div>
+
+
+    <div className="flex flex-row md:items-center">
+      <img
+        src="./cp.png"
+        onClick={toggleNewPost}
+        className="w-12 cursor-pointer p-2 hover:p-3 transition-all ease-in-out"
+      />
+      <img
+        src="./search.png"
+        onClick={toggleSearchUser}
+        className="w-12 cursor-pointer p-2 hover:p-3 transition-all ease-in-out"
+      />
+      <img
+        src="./orbit.png"
+        onClick={toggleNotifications}
+        className="w-12 cursor-pointer p-2 hover:p-3 transition-all ease-in-out"
+      />
+      <img
+        src="./wormhole.png"
+        onClick={toggleDMList}
+        className="w-12 cursor-pointer p-2 hover:p-3 transition-all ease-in-out"
+      />
+      <img
+        src="./profile.png"
+        onClick={toggleProfile}
+        className="w-12 cursor-pointer p-2 hover:p-3 transition-all ease-in-out"
+      />
+    </div>
+  </div>
+);
+
+      
+      
+      
+      
+      
 
 }
 
