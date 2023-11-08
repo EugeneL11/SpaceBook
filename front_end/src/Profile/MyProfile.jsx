39278b71d6560fa6e6a855e5dee794894635b9e7
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-11-07 18:33:29 -0700

    Aesthetified profile page✨ [50min]

--- front_end/src/Profile/MyProfile.jsx
+++ front_end/src/Profile/MyProfile.jsx
@@ -5,9 +5,7 @@ import { Canvas } from "@react-three/fiber";
 
 function PlanetCanvas () {
     return (
-    <Canvas className="cursor-pointer mt-5 md:mt-0"
-        // camera={{ position: [0, 0, 1], fov: 50 }}
-        >
+    <Canvas className="cursor-pointer mt-5 md:mt-0">
         <Planet/>
     </Canvas>
     )
@@ -25,30 +23,25 @@ function Post(props) {
         setImageNum(nextImage);
     }
     return(
-        <div className="flex flex-col w-5/12 mx-auto mb-5 justify-center align-middle bg-slate-300 bg-opacity-90 text-black rounded-lg">
+        <div className="flex flex-col w-5/12 mx-auto mb-10 justify-center align-middle bg-slate-300 bg-opacity-90 text-black rounded-lg">
             <div className="relative w-100 h-100">
             {imageNum > 0 ? 
-                <div className="absolute text-green-300 text-9xl top-52 z-50 hover:text-green-400" onClick={togglePrevImage}> {"<"} </div> : null}
+                <div className="absolute text-purple-500 pb-2 pr-2 pl-2 bg-slate-300 bg-opacity-60 rounded-full text-7xl top-52 z-40 cursor-pointer hover:text-purple-400" onClick={togglePrevImage}> {"←"} </div> : null}
             {imageNum < imageCount - 1 ? 
-                <div className="absolute text-green-300 text-9xl left-85-percent top-52 z-50 hover:text-green-400" onClick={toggleNextImage}> {">"} </div> : null}
+                <div className="absolute text-purple-500 pb-2 pr-2 pl-2 bg-slate-300 bg-opacity-60 rounded-full text-7xl cursor-pointer left-85-percent top-52 z-40 hover:text-purple-400" onClick={toggleNextImage}> {"→"} </div> : null}
                 </div>
             <div className="flex flex-row p-3 justify-between">
-                <div className="flex flex-row justify-center align-middle ">
+                <div className="flex flex-row justify-center align-middle pl-2 pt-2 ">
                     <img src={props.post.pfp} alt="Profile Picture" className="w-10 aspect-square rounded-full"/>
                     <p className="ml-2 mt-2">{props.post.username}</p>
                 </div>
-                <p className="mr-2 mt-2">{props.post.date}</p>
+                <p className="mr-2 mt-2 pr-2 pt-2">{props.post.date}</p>
             </div>
 
-
-
-            <p className="mt-2 pl-5"> {props.post.caption} 😂</p>
-
-            <img src={props.post.images[imageNum]} className="m-4" alt="" />
-
-
+            <p className="mt-2 pl-5"> {props.post.caption}</p>
+            {/* no resize on image */}
+            <img src={props.post.images[imageNum]} className="m-4 h-80  object-contain " alt="" />
         </div>
-
     )
 }
 
@@ -61,16 +54,16 @@ function MyProfile(props) {
         username: "Duppy", 
         homePlanet: "Earth",
         pfp: "./ayylmao.webp",
-        bio: "My unmatched perspicacity coupled with sheer indefitigability makes me a feared opponent in any planet in this solar system.\n https://okhan.me"
+        bio: "My unmatched perspicacity coupled with sheer indefitigability makes me a feared opponent in any planet of this solar system.\n https://okhan.me/"
     }
     const [user, setUser] = useState(null)
     const [posts, setPosts] = useState(null)
     const examplePost = {
-        username: "duppy",
+        username: "Duppy",
         pfp : "./ayylmao.webp",
-        caption: "Finally Leaving this planet lmao",
+        caption: "Finally leaving this planet lmao 😂",
         date: "Nov 7th",
-        images: ["./ayylmao.webp", "./swag.jpg"],
+        images: ["./swag.jpg", "./ayylmao.webp"],
         videos: [],
     }
     const examplePosts =[ examplePost,examplePost]
@@ -84,7 +77,7 @@ function MyProfile(props) {
     return (
         <div className="flex flex-col">
 
-        <button className="mb-5 w-fit ml-6 text-3xl" onClick={toggleHomepage}> {'<--'} </button>
+        <button className="mb-5 w-fit ml-6 text-3xl hover:text-purple-300" onClick={toggleHomepage}> {'←'} </button>
 
         { user ? 
         
@@ -99,7 +92,7 @@ function MyProfile(props) {
                     <img src={user.pfp} alt="My Profile Picture" className="w-20 aspect-square rounded-full"/>
 
                     <div className="flex flex-col ml-4 justify-center align-middle">
-                        <p>{user.username}</p>
+                        <p className="text-xl">{user.username}</p>
                         <p>from {user.homePlanet}</p>
                     </div>
 
@@ -107,12 +100,12 @@ function MyProfile(props) {
 
                 <p className="mb-4">{user.bio}</p>
 
-                <button onClick={toggleSettings} className="flex flex-row cursor-pointer ">
+                <button onClick={toggleSettings} className="flex flex-row cursor-pointer mb-3 hover:text-purple-300">
                     <img src="./gear.png" className="h-5 aspect-square translate-y-0.5 mr-2"/>
                     <p>Edit profile settings</p>
                 </button>
 
-                <button onClick={toggleFriendsList} className="flex flex-row cursor-pointer ">
+                <button onClick={toggleFriendsList} className="flex flex-row cursor-pointer hover:text-purple-300">
                     <img src="./sun.png" className="h-5 aspect-square translate-y-0.5 mr-2"/>
                     <p>View orbit buddies</p>
                 </button>
