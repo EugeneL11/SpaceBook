43f9110921347d447efeeb4fabffbfca0542bda6
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-11-09 22:02:11 -0700

    Kevin[1min]:add modified version of Omar's (actually Victor's) login fetch test

--- front_end/src/Login/login.jsx
+++ front_end/src/Login/login.jsx
@@ -1,37 +1,58 @@
 import { React, useState } from "react";
-import {userID} from "../Static.js"
+import { userID } from "../Static.js";
 function Login(props) {
-    const toggleHomepage = props.toggleHomepage
-    const toggleRegister = props.toggleRegister
+    const toggleHomepage = props.toggleHomepage;
+    const toggleRegister = props.toggleRegister;
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    const loginAction = () =>{
+    const loginAction = async () => {
         //ask backend
-        toggleHomepage()
-    }
+        const result = await fetch("http://localhost:8080/ping");
+
+        const res = await result.json();
+        console.log(res);
+        toggleHomepage();
+    };
 
     return (
-    <div className="flex flex-col items-center">
-        <h1 className="text-6xl py-11">SpaceBook</h1>
+        <div class="flex flex-col items-center">
+            <h1 class="text-6xl py-11">SpaceBook</h1>
 
-        <div className="bg-white text-black text-center text-xl p-10 sm:p-14 rounded-md w-2/3 sm:w-1/2 lg:w-2/5 xl:w-1/3 min-w-fit">
-            <h4 className="text-3xl mb-10">Welcome Back!</h4>
-            <div className="w-full">
-                <label htmlFor="username" className="block mb-2 text-left">Username</label>
-                <input className="w-full border-b-2 border-gray-700 focus:outline-none focus:border-gray-300 focus:ring-0" placeholder="Enter Username" type="text" value={username} onChange={e => setUsername(e.target.value)}></input>
+            <div class="bg-white text-black text-center text-xl p-10 sm:p-14 rounded-md w-2/3 sm:w-1/2 lg:w-2/5 xl:w-1/3 min-w-fit">
+                <h4 class="text-3xl mb-10">Welcome Back!</h4>
+                <div class="w-full">
+                    <label for="username" class="block mb-2 text-left">
+                        Username
+                    </label>
+                    <input
+                        class="w-full border-b-2 border-gray-700 focus:outline-none focus:border-gray-300 focus:ring-0"
+                        placeholder="Enter Username"
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}></input>
+                </div>
+                <div class="w-full mt-8">
+                    <label for="password" class="block mb-2 text-left">
+                        Password
+                    </label>
+                    <input
+                        class="w-full border-b-2 border-gray-700 focus:outline-none focus:border-gray-300 focus:ring-0"
+                        placeholder="Enter Password"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}></input>
+                </div>
+                <button class="bg-purple-200 px-7 py-3 mt-10 rounded-lg" onClick={loginAction}>
+                    Log In
+                </button>
             </div>
-            <div className="w-full mt-8">
-                <label htmlFor="password" className="block mb-2 text-left">Password</label>
-                <input className="w-full border-b-2 border-gray-700 focus:outline-none focus:border-gray-300 focus:ring-0" placeholder="Enter Password" type="password" value={password} onChange={e => setPassword(e.target.value)}></input>
-            </div>
-            <button className="bg-purple-200 px-7 py-3 mt-10 rounded-lg" onClick={loginAction}>Log In</button>
+
+            <button class="text-xl mt-12" onClick={toggleRegister}>
+                Sign Up
+            </button>
         </div>
-
-        <button className="text-xl mt-12" onClick={toggleRegister}>Sign Up</button>
-    </div>
     );
-
 }
 
 export default Login;
