816f48ba3e73be591d52e59e0889303964d372a3
EugeneL11 <eugenelee0605@gmail.com>
2023-11-14 14:29:47 -0700

    Eugene [30min]: Polish new post and edit expanded post

--- front_end/src/Posts/ExpandedPost.jsx
+++ front_end/src/Posts/ExpandedPost.jsx
@@ -1,5 +1,6 @@
 import {admin} from "../Static.js"
 import { React, useState, useEffect } from "react";
+import pPic from '../images/pp.png';
 function ExpandedPost(props) {
     const postID = props.postID
     const exampleFriends = ["Kevin", "Omar" , "Raine", "Eugene"]
@@ -38,7 +39,22 @@ function ExpandedPost(props) {
     <div className="flex flex-col items-center">
         <button className="mb-5 w-fit ml-10 mt-5 mr-auto text-5xl hover:text-purple-300" onClick={toggleHomepage}> {'←'} </button>
         <div className="flex flex-col bg-white text-black text-start text-lg m-5 md:py-6 sm:px-16 lg:px-24 p-6 rounded-xl w-3/4 md:w-1/2 min-w-fit">
-
+            <div className="flex flex-row justify-between items-center mt-8">
+                <div className="flex flex-row justify-center items-center align-middle">
+                    <img src="" alt="Profile Picture" className="w-10 aspect-square rounded-full"/>
+                    <p className="ml-4">Eugene</p>
+                </div>
+                <p className="">Nov. 14th</p>
+            </div>
+            <p className="mt-10">This is the caption......</p>
+            <div className="w-full bg-purple-200 rounded-lg p-2 my-2">
+                <img src={pPic} className="my-4 mx-auto h-80 object-contain" alt="the post picture"/>
+            </div>
+            <div className="flex flex-col w-full bg-purple-200 rounded-xl p-2 my-5">
+                <div className="w-full bg-purple-400 rounded-lg p-2 my-2">Add comment...</div>
+                <div className="w-full bg-purple-400 rounded-lg p-2 my-2">I don't give up that easily</div>
+                <div className="w-full bg-purple-400 rounded-lg p-2 my-2">Go to the Sun, it's got a warmer climate</div>
+            </div>
         </div>
     </div>
 
--- front_end/src/Posts/NewPost.jsx
+++ front_end/src/Posts/NewPost.jsx
@@ -18,46 +18,41 @@ function NewPost(props) {
     
     return (
         <div className="flex flex-col items-center">
-            <button className="mb-5 w-fit ml-6 text-3xl hover:text-purple-300" onClick={toggleHomepage}> {'←'} </button>
+            <button className="mb-5 w-fit ml-6 mr-auto text-3xl hover:text-purple-300" onClick={toggleHomepage}> {'←'} </button>
 
             <div class="flex flex-col bg-white text-black text-start text-lg m-10 md:py-6 sm:px-16 lg:px-24 p-6 rounded-xl w-3/4 md:w-1/2 min-w-fit">
-            <div class="flex justify-between items-center">
-                <h1 class="text-center text-3xl py-4">New Post</h1>
+                <div class="flex justify-center items-center">
+                    <h1 class="text-center text-3xl py-4">New Post</h1>
+                </div>
+
+                <label class=" mt-3 lg:mt-6">Write a caption </label>
+                <textarea class="form-textarea border-2 border-gray-700 focus:outline-none focus:border-gray-300 focus:ring-0" rows="3" placeholder=" Write a caption..."></textarea>
+            
+                <div class="mt-6">Add images </div>
+                <input type="file" class="form-input text-sm"></input>
+
+                <div className="relative w-100 h-100 mt-5 border-black border-2">
+                    {/* {imageNum > 0 ? 
+                        // <div className="absolute text-purple-500 pb-2 pr-2 pl-2 bg-slate-300 bg-opacity-60 rounded-full text-7xl top-52 z-40 cursor-pointer hover:text-purple-400" onClick={togglePrevImage}> {"←"} </div> : null
+                        <img src="./ar.png" className="absolute w-10 p-2 bg-slate-300 bg-opacity-80 rounded-full text-7xl top-52 z-40 cursor-pointer translate-x-10 translate-y-10 rotate-180" onClick={togglePrevImage} /> : null
+                    } */}
+
+                    <img src="./ar.png" className="absolute w-10 p-2 bg-slate-300 bg-opacity-80 rounded-full text-7xl top-52 z-40 cursor-pointer translate-x-5 -translate-y-16 rotate-180" onClick={toggleHomepage} /> 
+                    <img src="./swag.jpg" className="h-80 object-contain ml-auto mr-auto" alt="" />
+                    <img src="./ar.png" className="absolute w-10 p-2 bg-slate-300 bg-opacity-80 rounded-full text-7xl top-52 z-40 cursor-pointer right-0 -translate-x-5 -translate-y-16"  onClick={toggleHomepage}/>
+
+
+                    {/* {imageNum < imageCount - 1 ? 
+                    // <div className="absolute text-purple-500 pb-2 pr-2 pl-2 bg-slate-300 bg-opacity-60 rounded-full text-7xl cursor-pointer left-85-percent top-52 z-40 hover:text-purple-400" onClick={toggleNextImage}> {"→"} </div> : null
+                    <img src="./ar.png" className="absolute w-10 p-2 bg-slate-300 bg-opacity-80 rounded-full text-7xl top-52 z-40 cursor-pointer right-0 -translate-x-10 translate-y-10"  onClick={toggleNextImage}/> : null
+                    } */}
+                </div>
+
+                <button class="bg-red-300 px-2 py-1 mt-5 w-fit self-center rounded-md text-sm">Remove Selected Image</button>
+
+                <button class="bg-purple-300 px-7 py-3 mt-6 w-fit self-center rounded-lg">Post!</button>
+
             </div>
-
-            <label class=" mt-3 lg:mt-6">Write a caption </label>
-            <textarea class="form-textarea border-2 border-gray-700 focus:outline-none focus:border-gray-300 focus:ring-0" rows="3" placeholder="Write a caption..."></textarea>
-        
-            <div class="mt-6">Add images </div>
-            <input type="file" class="form-input text-sm"></input>
-
-            <div className="relative w-100 h-100 mt-5 border-black border-2">
-                {/* {imageNum > 0 ? 
-                    // <div className="absolute text-purple-500 pb-2 pr-2 pl-2 bg-slate-300 bg-opacity-60 rounded-full text-7xl top-52 z-40 cursor-pointer hover:text-purple-400" onClick={togglePrevImage}> {"←"} </div> : null
-                    <img src="./ar.png" className="absolute w-10 p-2 bg-slate-300 bg-opacity-80 rounded-full text-7xl top-52 z-40 cursor-pointer translate-x-10 translate-y-10 rotate-180" onClick={togglePrevImage} /> : null
-                } */}
-
-                <img src="./ar.png" className="absolute w-10 p-2 bg-slate-300 bg-opacity-80 rounded-full text-7xl top-52 z-40 cursor-pointer translate-x-5 -translate-y-16 rotate-180" onClick={toggleHomepage} /> 
-                <img src="./swag.jpg" className="h-80 object-contain ml-auto mr-auto" alt="" />
-                <img src="./ar.png" className="absolute w-10 p-2 bg-slate-300 bg-opacity-80 rounded-full text-7xl top-52 z-40 cursor-pointer right-0 -translate-x-5 -translate-y-16"  onClick={toggleHomepage}/>
-
-
-                {/* {imageNum < imageCount - 1 ? 
-                // <div className="absolute text-purple-500 pb-2 pr-2 pl-2 bg-slate-300 bg-opacity-60 rounded-full text-7xl cursor-pointer left-85-percent top-52 z-40 hover:text-purple-400" onClick={toggleNextImage}> {"→"} </div> : null
-                <img src="./ar.png" className="absolute w-10 p-2 bg-slate-300 bg-opacity-80 rounded-full text-7xl top-52 z-40 cursor-pointer right-0 -translate-x-10 translate-y-10"  onClick={toggleNextImage}/> : null
-                } */}
-            </div>
-
-
-  
-
-            <button class="bg-red-300 px-2 py-1 mt-5 w-fit rounded-md text-sm">Remove selected image</button>
-
-
-
-            <button class="bg-purple-300 px-7 py-3 mt-6 w-fit self-center rounded-lg">Post!</button>
-
-        </div>
 
         </div>
     ); 
