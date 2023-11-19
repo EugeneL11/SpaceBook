478f95cab7d000091aaeb59b365de0f934f56fb0
Victor Han <victor.han72@gmail.com>
2023-11-19 11:21:53 -0700

    Refactor File System

---
+++ back_end/handlers/Posts.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getHomePagePost() {
+
+}
+
+func HomepageHandler(ctx *gin.Context) {
+
+}
