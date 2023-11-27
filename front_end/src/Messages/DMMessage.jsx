df1f0976bfb11e57b91a6fd4a024ebc95d43247a
EugeneL11 <eugenelee0605@gmail.com>
2023-11-26 18:35:31 -0700

    Eugene [90min]: Make margins and buttons consistent

--- front_end/src/Messages/DMMessage.jsx
+++ front_end/src/Messages/DMMessage.jsx
@@ -42,10 +42,10 @@ function DMMessage(props) {
     return (
     <div className="flex flex-col items-center min-h-screen">
 
-        <button className="w-fit ml-10 mr-auto text-5xl hover:text-purple-300" onClick={toggleDMList}> {'←'} </button>
+        <button className="mb-5 w-fit ml-6 mr-auto text-3xl hover:text-purple-300" onClick={toggleDMList}> {'←'} </button>
 
         {messages ? (
-            <div className="bg-white min-h-[70%] w-3/4 md:w-1/2 min-w-fit mt-[-25px] pt-6 pb-12 px-16 rounded-xl">
+            <div className="bg-white min-h-[70%] w-full sm:w-3/4 lg:w-1/2 min-w-fit mt-[-20px] pt-6 pb-12 px-10 lg:px-16 rounded-xl">
                 <div className="bg-purple-700 w-full rounded-md py-5 mb-10 flex justify-center items-center">
                     <div onClick={toggleOtherProfile} className="text-white text-3xl absolute hover:cursor-pointer">{friendID}</div>
                 </div>
