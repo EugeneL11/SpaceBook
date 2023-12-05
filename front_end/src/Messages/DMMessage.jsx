496544341371c2edd29ed1ee8423356e0a958043
EugeneL11 <eugenelee0605@gmail.com>
2023-12-04 18:41:12 -0700

    Eugene [10 min]: load more messages button

--- front_end/src/Messages/DMMessage.jsx
+++ front_end/src/Messages/DMMessage.jsx
@@ -72,15 +72,12 @@ function DMMessage(props) {
     }
     return (
     <div className="flex flex-col items-center min-h-screen">
-
         <button className="mb-5 w-fit ml-6 mr-auto text-3xl hover:text-purple-300" onClick={toggleDMList}> {'←'} </button>
-            <div> {maxSubSet === false ? <div onClick={loadMore}> Load more messages</div>: <div>dasdasd</div>}</div>
             <div className="bg-white min-h-[70%] w-full sm:w-3/4 lg:w-1/2 min-w-fit mt-[-20px] pt-6 pb-12 px-10 lg:px-16 rounded-xl">
                 <div className="bg-purple-700 w-full rounded-md py-5 mb-10 flex justify-center items-center">
                     <div onClick={toggleOtherProfile} className="text-white text-3xl absolute hover:cursor-pointer">{friendUsername}</div>
                 </div>
-                
-               
+                 
                 {messages ? (
                     <div className="flex flex-col gap-1">
                         {messages.map((message, index) => (
@@ -102,6 +99,13 @@ function DMMessage(props) {
                     <button onClick={sendMessage} className="ml-2 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Send</button>
                 </div>
             </div>
+
+        <div className="my-10 text-white"> 
+            {maxSubSet === false ? 
+                <button onClick={loadMore} className="p-3 bg-purple-400 hover:bg-purple-200 rounded-lg">Load More</button> : 
+                <button className="">this does nothing</button>
+            }
+        </div>
     </div>
     );
 }
